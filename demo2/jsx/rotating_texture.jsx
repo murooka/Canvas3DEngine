be1abe29f8236159fb83f35b7679f0aa81adfa8c
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-10-19 03:02:18 +0900

    Simplify demo2

--- demo2/jsx/rotating_texture.jsx
+++ demo2/jsx/rotating_texture.jsx
@@ -5,32 +5,29 @@ import "../../lib/engine.jsx";
 final class _Main {
     static function main(args:string[]) : void {
 
-        var imageName = './image/so-nya.png';
-
-        Engine.loadImages([imageName]);
         var engine = new Engine('canvas');
 
         engine.camera.view = new Vector(0, 0, -100);
         engine.camera.target = new Vector(0, 0, 0);
         engine.camera.updateMatrix();
 
-        var q = Quaternion.rotating(0, 0, 0, 0);
+        var q = Quaternion.rotating(0, 0, 0.7, 0.7);
 
         engine.onUpdate = (elapsedMsec:number):void -> {
-            q.mulSelf(Quaternion.rotating(Math.PI/100, 0, 0, 0.19));
+            q.mulSelf(Quaternion.rotating(Math.PI/100, 0, 0.7, 0.7));
         };
         engine.onRender = (context:Context3D, elapsedMsec:number):void -> {
             context.setBackgroundColor(new Color(190, 240, 255));
+
             context.pushMatrix();
             context.rotate(q);
-            context.renderTexture([
-                new Vector(-10,-10,  0),
-                new Vector( 10,-10,  0),
+            context.renderPolygon([
+                new Vector(-10, 10,  0),
                 new Vector( 10, 10,  0),
-                new Vector(-10, 10,  0)
-            ], imageName, 4, 4, 3, 2);
+                new Vector( 10,-10,  0),
+                new Vector(-10,-10,  0)
+            ], new Color(255, 159, 79));
             context.popMatrix();
-
         };
 
         engine.start();
